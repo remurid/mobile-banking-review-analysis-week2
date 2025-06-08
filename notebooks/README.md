9f52415d58244f48790320af9a082bff99c1a24a
remurid <rexrid1@gmail.com>
2025-06-08 18:23:42 +0300

    Add initial project structure with .gitignore, README files, and empty __init__.py files

---
+++ notebooks/README.md
@@ -0,0 +1,4 @@
+# Notebooks
+
+
+Run these notebooks after setting up your environment as described in the main README.
---
+++ src/utils.py
@@ -0,0 +1,4 @@
+from google_play_scraper import Sort, reviews
+from google_play_scraper import reviews_all
+import pandas as pd
+from datetime import datetime
